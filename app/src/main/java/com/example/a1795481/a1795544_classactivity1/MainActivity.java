4d883c990ff4a9b8f2af93163ba6deb5b0eac025
1795481 <rajuchahal0123@gmail.com>
2019-06-03 16:55:46 -0400

    meassage

--- app/src/main/java/com/example/a1795481/a1795544_classactivity1/MainActivity.java
+++ app/src/main/java/com/example/a1795481/a1795544_classactivity1/MainActivity.java
@@ -9,5 +9,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        EditText edt_name,edt_email,edt_pass;
+        Button btn_sub;
+        static String TAG = "my info";
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_classwork);
+
+            edt_name = findViewById(R.id.edt_name);
+            edt_email = findViewById(R.id.edt_email);
+            edt_pass = findViewById(R.id.edt_numpass);
+
+            btn_sub = findViewById(R.id.btn_submit);
+
+            btn_sub.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String name = edt_name.getText().toString();
+                    String email = edt_email.getText().toString();
+
+                    Log.v(TAG, name);
+                    Log.v(TAG, email);
+                    Log.v(TAG, edt_pass.getText().toString());
+
+                }
+            });
+
+
+        }
